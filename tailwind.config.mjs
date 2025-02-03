7b811490511724b9fd0c350aabd2bbc720a08435
mustafo-coder <shaeduloffdev@gmail.com>
2025-02-03 18:15:33 +0500

    first

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#1E5CEA',
+				secondary: '#E6EFFD',
+			},
+			container: {
+				padding: 10,
+				center: true,
+			},
+		},
+	},
+	plugins: [],
 };
